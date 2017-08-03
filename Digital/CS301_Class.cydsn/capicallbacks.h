1426b6a95ed7237865a548cce77807a2ffbdcc4c
TheGuardianWolf <nano@pixelcollider.net>
2017-08-04 07:22:47 +1200

    Debug updated with LED controls.

---
+++ Digital/CS301_Class.cydsn/capicallbacks.h
@@ -0,0 +1,18 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#include "debug.h"
+
+#define USBFS_DISPATCH_CDC_CLASS_CDC_READ_REQUESTS_CALLBACK led_set(LED_STATE_ON);
+// #define USBFS_EP_0_ISR_ENTRY_CALLBACK (void) 0;
+
+/* [] END OF FILE */
--- Digital/CS301_Class.cydsn/debug.h
+++ Digital/CS301_Class.cydsn/debug.h
@@ -1,6 +1,14 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-void log_usb(char[] message);
+#include <stdint.h>
+    
+#define LED_STATE_ON 1
+#define LED_STATE_OFF 0
+#define LED_STATE_TOGGLE 2
+    
+void led_set(uint8_t state);
+    
+void log_usb(char* message);
 
 #endif /* DEBUG_H */
