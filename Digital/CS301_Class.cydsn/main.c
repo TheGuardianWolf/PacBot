ce37642eea29765c00d2863ef345cde02b8b29cc
TheGuardianWolf <nano@pixelcollider.net>
2017-08-14 17:30:20 +1200

    Pre-calibration circuit, mux input is op-amp buffered.

--- Digital/CS301_Class.cydsn/main.c
+++ Digital/CS301_Class.cydsn/main.c
@@ -23,47 +23,45 @@
 #include "wireless.h"
 #include "motor.h"
 #include "quad_dec.h"
-#include "debug.h"
-#include "pid.h"
 #include "analog.h"
 #include "systime.h"
 //* ========================================
 
-void mux_test() {
-    if(SW_Read() == 0) {
-        led_set(LED_STATE_ON);
-        while(SW_Read() == 0) {
-        }
-        mux_next();
-    }
-    else {
-        led_set(LED_STATE_OFF);
-    }
-}
+// void mux_test() {
+//     if(SW_Read() == 0) {
+//         led_set(LED_STATE_ON);
+//         while(SW_Read() == 0) {
+//         }
+//         mux_next();
+//     }
+//     else {
+//         led_set(LED_STATE_OFF);
+//     }
+// }
 
-void pk_drain_test() {
-    if(SW_Read() == 0) {
-        led_set(LED_STATE_ON);
-        while(SW_Read() == 0) {
-        }
-        PK_DRAIN_Write(~PK_DRAIN_Read());
-    }
-    else {
-        led_set(LED_STATE_OFF);
-    }
-}
+// void pk_drain_test() {
+//     if(SW_Read() == 0) {
+//         led_set(LED_STATE_ON);
+//         while(SW_Read() == 0) {
+//         }
+//         PK_DRAIN_Write(~PK_DRAIN_Read());
+//     }
+//     else {
+//         led_set(LED_STATE_OFF);
+//     }
+// }
 
-void light_array_test() {
-    if(switch_get()) {
-        led_set(LED_STATE_ON);
-        while(switch_get() == 0) {
-        }
-        adc_solo_collect();
-    }
-    else {
-        led_set(LED_STATE_OFF);
-    }
-}
+// void light_array_test() {
+//     if(switch_get()) {
+//         led_set(LED_STATE_ON);
+//         while(switch_get() == 0) {
+//         }
+//         adc_solo_collect();
+//     }
+//     else {
+//         led_set(LED_STATE_OFF);
+//     }
+// }
 
 void system_init() {
     USE_RF;
@@ -80,11 +78,11 @@ int main()
     //motor_set(33, 33);
 //    M1_QuadDec_SetCounter(QUADDEC_MAX - 10);
 //    M2_QuadDec_SetCounter(QUADDEC_MAX - 10);
-    uint16_t average;
+    // uint16_t average;
     system_init();
     for(;;) {
-        light_array_test();
-        average = adc_solo_get();
+        // light_array_test();
+        // average = adc_solo_get();
     }   
 }
 
