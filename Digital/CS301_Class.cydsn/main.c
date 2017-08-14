0a33a29772d937b8836f5de510e46f16bfcfb932
TheGuardianWolf <nano@pixelcollider.net>
2017-08-14 18:16:25 +1200

    Implemented interview PWM showcase.

--- Digital/CS301_Class.cydsn/main.c
+++ Digital/CS301_Class.cydsn/main.c
@@ -1,31 +1,16 @@
-/* ========================================
- * Fully working code: 
- * PWM      : 
- * Encoder  : 
- * ADC      :
- * USB      : port displays speed and position.
- * CMD: "PW xx"
- * Copyright Univ of Auckland, 2016
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF Univ of Auckland.
- *
- * ========================================
-*/
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <project.h>
-//* ========================================
+
 #include "usb.h"
 #include "wireless.h"
 #include "motor.h"
 #include "quad_dec.h"
 #include "analog.h"
+#include "debug.h"
 #include "systime.h"
-//* ========================================
+
 
 // void mux_test() {
 //     if(SW_Read() == 0) {
@@ -62,27 +47,42 @@
 //         led_set(LED_STATE_OFF);
 //     }
 // }
+static uint8_t pwm_mode = 0;
+void pwm_test() {
+    if(switch_get()) {
+        while(switch_get() == 0) {
+        }
+        pwm_mode++;
+        if (pwm_mode == 3) {
+            pwm_mode = 0;
+        }
+        if (pwm_mode == 0) {
+            motor_set(M_DRIFT, M_DRIFT);
+        }
+        else if (pwm_mode == 1) {
+            motor_set(M_MAX / 2, M_MAX / 2);
+        }
+        else if (PWM_mode == 2) {
+            motor_set(M_MAX, M_MAX);
+        }
+    }
+}
 
 void system_init() {
+    systime_init();
+    analog_init();
     USE_RF;
     motor_init();
     usb_init();
     quad_dec_init();
-    analog_init();
-    systime_init();
     CYGlobalIntEnable;
 }
 
 int main()
 {
-    //motor_set(33, 33);
-//    M1_QuadDec_SetCounter(QUADDEC_MAX - 10);
-//    M2_QuadDec_SetCounter(QUADDEC_MAX - 10);
-    // uint16_t average;
     system_init();
     for(;;) {
-        // light_array_test();
-        // average = adc_solo_get();
+        pwm_test();
     }   
 }
 
