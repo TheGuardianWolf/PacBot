374492ac5f03460dc661b922ce1c7547acfa798a
TheGuardianWolf <nano@pixelcollider.net>
2017-08-29 19:29:17 +1200

    Added more comments.

--- Digital/CS301_Class.cydsn/main.c
+++ Digital/CS301_Class.cydsn/main.c
@@ -10,9 +10,15 @@
 #include "usb.h"
 
 void system_init() {
+    // Starting the IAMP opamp only. If issues are encountered, set the input
+    // of IAMP to pull-down resistive.
+    IAMP_Start();
+
     motor_controller_init();
     adc_init();
     CYGlobalIntEnable;
+
+    // If system does not initialise (LED does not light up), this is likely the issue.
     adc_wait_ready();
 }
 
@@ -20,8 +26,9 @@ int main() {
     system_init();
     MCData mcd = motor_controller_create();
     while(true) {
+        // Without button press, default to RF and ADC output mode
+        // With button press, go to straight line test
         if(btn_get()) {
-            // Forward by 1000mm
             led_set(0b000);
             while(btn_get());
             if(!btn_get()) {
@@ -30,15 +37,21 @@ int main() {
                 while (td < 2000){
                     td = systime_ms() - now;
                 }
+                // Signature is MCData, left wheel distance (mm), right wheel distance (mm)
                 motor_controller_run_forward(&mcd, 1100, 1100);
+                // This function blocks intentionally, reset the robot power after reached.
             }
             
         }
         else {
+            // RF per 1 second, ADC per 2 seconds.
             led_set(0b001);
             uint32_t now = systime_s();
             uint32_t start_adc = now;
             uint32_t start_rf = now;
+            // RF polling function can run here
+
+
             if (now - start_adc >= 2) {
                 start_adc = now;
                 char packet[64];
@@ -49,9 +62,9 @@ int main() {
             if (now - start_rf >= 1) {
                 start_rf = now;
                 char packet[64];
-                ADCData adc_data = adc_get();
-                sprintf(packet, "V:%.3u A:%.3u");
-                usb_send_string(packet);
+                // Put RF formatting crap here and sprintf it into the packet
+                //sprintf(packet, "V:%.3u A:%.3u");
+                //usb_send_string(packet);
             }
         }  
     }
