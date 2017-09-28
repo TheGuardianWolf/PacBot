234858942c0b99bde30d92682e459bbaa0a5b995
TheGuardianWolf <nano@pixelcollider.net>
2017-09-28 13:57:38 +1300

    Revert to old isrs

--- Digital/CS301_Class.cydsn/system/sensors.c
+++ Digital/CS301_Class.cydsn/system/sensors.c
@@ -31,11 +31,11 @@ static int8_t next_mux() {
 
 static void line_fsm_start() {
     REG_DRAIN_Write(0b11);
-    SIGTIMER_RESET_Write(0b01);
-    isr_SIGTIMER_RISE_ClearPending();
-    isr_SIGTIMER_RISE_Enable();
     mux_selection = next_mux();
     SIGMUX_FastSelect(mux_selection);
+    SIGTIMER_RESET_Write(0b1);
+    isr_SIGTIMER_ClearPending();
+    isr_SIGTIMER_Enable();
 }
 
 static void line_fsm(uint8_t trigger) {  // 0 for line_timer, 1 for line_rise, 2 for line_fall
@@ -43,18 +43,17 @@ static void line_fsm(uint8_t trigger) {  // 0 for line_timer, 1 for line_rise, 2
     switch(line_fsm_state) {
         case 0:
         if (trigger == 0) {
-            isr_SIGTIMER_RISE_Disable();
             REG_DRAIN_Write(0b00);
             isr_SIGRISE_ClearPending();
             isr_SIGRISE_Enable();
-            SIGTIMER_RESET_Write(0b01);
-            isr_SIGTIMER_RISE_ClearPending();
-            isr_SIGTIMER_RISE_Enable();
+            SIGTIMER_RESET_Write(0b1);
+            isr_SIGTIMER_ClearPending();
+            isr_SIGTIMER_Enable();
             line_fsm_state = 1;
         }
         case 1:
         if (trigger == 0 || trigger == 1) {
-            isr_SIGTIMER_RISE_Disable();
+            isr_SIGTIMER_Disable();
             isr_SIGRISE_Disable();
             reading = REG_LINE_Read();
             line_data.state[mux_selection] = (bool) reading ^ line_invert.state[mux_selection];
@@ -90,22 +89,23 @@ static void line_fsm(uint8_t trigger) {  // 0 for line_timer, 1 for line_rise, 2
             isr_SIGFALL_ClearPending();
             isr_SIGFALL_Enable();
             REG_DRAIN_Write(0b11);
-            SIGTIMER_RESET_Write(0b10);
-            isr_SIGTIMER_FALL_ClearPending();
-            isr_SIGTIMER_FALL_Enable();
+            SIGTIMER_RESET_Write(0b1);
+            isr_SIGTIMER_ClearPending();
+            isr_SIGTIMER_Enable();
             line_fsm_state = 2;
         }
         break;
         case 2:
         if (trigger == 0 || trigger == 2) {
-            isr_SIGTIMER_FALL_Disable();
+            isr_SIGTIMER_Disable();
             isr_SIGFALL_Disable();
             REG_DRAIN_Write(0b00);
             isr_SIGRISE_ClearPending();
             isr_SIGRISE_Enable();
-            SIGTIMER_RESET_Write(0b01);
-            isr_SIGTIMER_RISE_ClearPending();
-            isr_SIGTIMER_RISE_Enable();
+            SIGTIMER_RESET_Write(0b1);
+            isr_SIGTIMER_ClearPending();
+            isr_SIGTIMER_Enable();
+            mux_selection = SIGMUX_GetChannel();
             line_fsm_state = 1;
         }
         break;
@@ -132,6 +132,7 @@ CY_ISR(line_fall) {
 
 void sensors_init() {
     SIGMUX_Start();
+    SIGMUX_Next();
     SMUX_BUF_Start();
     SAMP_Start();
     IAMP_Start();
@@ -142,16 +143,13 @@ void sensors_init() {
     PKCOMP_REF_HIGH_Start();
     PKCOMP_REF_LOW_SetValue(LINE_LOW);
     PKCOMP_REF_HIGH_SetValue(LINE_HIGH);
-    isr_SIGTIMER_RISE_StartEx(line_timer_rise);
-    isr_SIGTIMER_RISE_Disable();
-    isr_SIGTIMER_FALL_StartEx(line_timer_fall);
-    isr_SIGTIMER_FALL_Disable();
+    isr_SIGTIMER_StartEx(line_timer);
+    isr_SIGTIMER_Disable();
     isr_SIGRISE_StartEx(line_rise);
     isr_SIGRISE_Disable();
     isr_SIGFALL_StartEx(line_fall);
     isr_SIGFALL_Disable();
-    SIGTIMER_RISE_Start();
-    SIGTIMER_FALL_Start();
+    SIGTIMER_Start();
     line_fsm_start();
     // Wait for first set of readings.
     //while (!line_init);
