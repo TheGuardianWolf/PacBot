6760ce5162066014acb4dbb2dc1456aa676c1d00
Jerry <nano@pixelcollider.net>
2017-10-10 11:31:15 +1300

    Dealing with a backtrace bug.

--- Matlab/adj-list-a-star/algorithms/graph_travel_all.m
+++ Matlab/adj-list-a-star/algorithms/graph_travel_all.m
@@ -9,8 +9,9 @@ function [ path ] = graph_travel_all( graph, start )
 end
 
 function make_acyclic(graph, start)
+    % Modified DFS to break cyclic branches at immediate root
     frontier = java.util.LinkedList;
-    frontier.add(start);
+    frontier.push(start);
     came_from = cell(1, length(graph.nodes));
     came_from{start} = 0;
     
@@ -25,12 +26,41 @@ function make_acyclic(graph, start)
                 frontier.push(next);
             else
                 backtrace_edge = came_from{current};
-                backtrace = came_from{current}.get_arc_from(current);
-                while (backtrace ~= start && graph.node_order(backtrace) < 3)
-                    backtrace_edge = came_from{backtrace};
-                    backtrace = backtrace_edge.get_arc_from(backtrace);
+                backtrace = backtrace_edge.get_arc_from(current);
+                if (backtrace ~= next)
+                    came_from{current} = [];
+                    while (backtrace ~= start && graph.node_order(backtrace) < 3)
+                        backtrace_edge = came_from{backtrace};
+                        backtrace = backtrace_edge.get_arc_from(backtrace);
+                        came_from{backtrace} = [];
+                    end
+                    graph.edge_remove(backtrace_edge);
+                    break;
                 end
-                graph.edge_remove(backtrace_edge);
+            end
+        end
+    end
+end
+
+function path = make_path(graph, start)
+    path = cell(1, ceil(length(graph.nodes) * 1.5));
+    % Modified BFS to repath shorter branches
+    frontier = java.util.LinkedList;
+    frontier.add(start);
+    came_from = cell(1, length(graph.nodes));
+    came_from{start} = 0;
+    
+    while frontier.size() ~= 0
+        current = frontier.remove();
+        edges = graph.edges{current};
+        
+        % if current's order is 1.... backtrack and insert reverse path
+        
+        for i=1:length(edges)
+            [next, ~] = edges{i}.get_arc_from(current);
+            if isempty(came_from{next})
+                came_from{next} = edges{i};
+                frontier.add(next);
             end
         end
     end
