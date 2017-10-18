1220d5a0458bdc92c928005ed9a9a37336e76e38
Hanliang Ding <hdin898@aucklanduni.ac.nz>
2017-10-18 17:43:21 +1300

    more graph test

--- Digital/CS301_Class.cydsn/tests/test_graph.c
+++ Digital/CS301_Class.cydsn/tests/test_graph.c
@@ -1,9 +1,23 @@
-#include "minunit.h"
-#include "graph.h"
 #include <stdio.h>
+#include "minunit.h"
+#include "vector.h"
+#include <stddef.h>
 
+int tests_run = 0;
 
+int foo = 7;
+int bar = 4;
+int baz = 1;
+int* bof = &foo;
 
+static char * test_graph_create() {
+	uint8_t grid[5][5];
+    Graph* gra = graph_create(grid , 10, 10);
+    mu_assert("test_vector_create: error, vector->capacity != 4", vec->capacity == 4);
+    mu_assert("test_vector_create: error, vector->size != 0", vec->size == 0);
+    vector_destroy(vec);
+    return 0;
+}
 
 
 static char * all_tests() {
